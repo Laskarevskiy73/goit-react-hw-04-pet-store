19c950d830bdd9c7c0746207dffd510198164166
Laskarevskiy Andrey <laskarevskiy73@gmail.com>
2019-07-23 22:08:48 +0300

    add spiner with a long load page

--- src/LoadableLoadingPage.js
+++ src/LoadableLoadingPage.js
@@ -1,22 +1,30 @@
+import React from 'react';
 import Loadable from 'react-loadable';
-import Loader from './components/Loader';
+import Spinkit from 'react-spinkit';
+import style from './components/css/Spinkit.module.css';
+
+const Loading = () => (
+  <div className={style.baseStyleContainer}>
+    <Spinkit name="ball-scale-multiple" className={style.loading} />
+  </div>
+);
 
 export const Home = Loadable({
   loader: () => import('./pages/Home'),
-  loading: Loader,
+  loading: Loading,
 });
 
 export const Pets = Loadable({
   loader: () => import('./pages/Pets'),
-  loading: Loader,
+  loading: Loading,
 });
 
 export const Pet = Loadable({
   loader: () => import('./pages/Pet'),
-  loading: Loader,
+  loading: Loading,
 });
 
 export const About = Loadable({
   loader: () => import('./pages/About'),
-  loading: Loader,
+  loading: Loading,
 });
